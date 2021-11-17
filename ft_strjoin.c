2d73b9814d7cf03aeb68d45b035882a7070b3152
mbarreto <92719243+mikaelbelmont@users.noreply.github.com>
2021-11-17 16:30:48 +0000

    Add files via upload

---
+++ ft_strjoin.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	int i = 0;
+	int j = 0;
+	char *str;
+
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
