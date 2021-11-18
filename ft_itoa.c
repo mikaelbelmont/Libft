966f672fb157148d344d9f72b4edadadd4df2a1e
mbarreto <92719243+mikaelbelmont@users.noreply.github.com>
2021-11-18 19:31:27 +0000

    Add files via upload

---
+++ ft_itoa.c
@@ -0,0 +1,41 @@
+#include "libft.h"
+
+int	numlen(int n)
+{
+	int	l = 1;
+
+	while (n > 9)
+	{
+		n = n / 10;
+		l++;
+	}
+	return (l);
+}
+
+char *ft_itoa(int n)
+{
+	char	*str;
+	int		s = 0;
+	int		n;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n < 0)
+	{
+		n = n * -1;
+		s = 1;
+	}
+	n = numlen(n);
+	str = (char *)malloc(sizeof(char ) * (n + s + 1));
+	if (!str)
+		return (NULL);
+	str[n + s] = '\0';
+	while (n-- > 0)
+	{
+		str[n + s] = 48 + n % 10;
+		n = n / 10;
+	}
+	if (s == 1)
+		str[n + s] = '-';
+	return (str);
+}
---
+++ ft_split.c
@@ -0,0 +1,81 @@
+#include "libft.h"
+
+static int count_cols(const char *s, char c, int i)
+{
+	int	n = 0;
+
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+		n++;
+	}
+	return (n);
+}
+
+static char **clean_matriz(char const **dst, int j)
+{
+	while (j > 0)
+	{
+		j--;
+		free((void *)dst[j]);
+	}
+	free(dst);
+	return (NULL);
+}
+
+static char **create_matriz(const char *s, char **dst, char c, int l)
+{
+	int	i = 0;
+	int	j = 0;
+	int	k;
+
+	while (s[i] != '\0' && j < l)
+	{
+		k = 0;
+		while (s[i] == c)
+			i++;
+		dst[j] = (char *)malloc(sizeof(char) * count_cols(s, c, i) + 1);
+		if (dst[j] == NULL)
+			return (clean_matriz((char const **)dst, j));
+		while (s[i] != '\0' && s[i] != c)
+			dst[j][k++] = s[i++];
+		dst[j][k] = '\0';
+		j++;
+	}
+	dst[j] = 0;
+	return (dst);
+}
+
+int ft_countwords(const char *s, char c)
+{
+	int i = 0;
+	int j = 0;
+	int f = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			f = 0;
+		else if (f == 0)
+		{
+			f = 1;
+			j++;
+		}
+		i++;
+	}
+	return (j);
+}
+
+char **ft_split(char const *s, char c)
+{
+	int		l;
+	char	**dst;
+
+	if (s == NULL)
+		return (NULL);
+	l = ft_countwords(s, c);
+	dst = (char **)malloc(sizeof(char *) * (l + 1));
+	if (dst == NULL)
+		return (NULL);
+	return (create_matriz(s, dst, c, l));
+}
